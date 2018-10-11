91c71404901b7d2c117141acf6aa2f4a4a602777
Geoff Gorham <geogor37@gmail.com>
2018-10-10 22:17:19 -0600

    Set up the layouts for the checkup screens

---
+++ app/src/main/java/com/logger/car/androidcarmaintenanceapp/checkup/CheckupGasFragment.kt
@@ -0,0 +1,13 @@
+package com.logger.car.androidcarmaintenanceapp.checkup
+
+import android.os.Bundle
+import android.support.v4.app.DialogFragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class CheckupGasFragment: DialogFragment() {
+	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+		return super.onCreateView(inflater, container, savedInstanceState)
+	}
+}
