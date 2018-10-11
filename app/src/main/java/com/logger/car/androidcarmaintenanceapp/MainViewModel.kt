baace0f60c6a9bf03c62db5653867e4fca78d3ff
Geoff Gorham <geogor37@gmail.com>
2018-10-10 21:26:09 -0600

    Removed unnecessary logic.

--- app/src/main/java/com/logger/car/androidcarmaintenanceapp/MainViewModel.kt
+++ app/src/main/java/com/logger/car/androidcarmaintenanceapp/MainViewModel.kt
@@ -1,6 +1,5 @@
 package com.logger.car.androidcarmaintenanceapp
 
-import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
 import com.logger.car.androidcarmaintenanceapp.domain.FluidLogEntry
 import com.logger.car.androidcarmaintenanceapp.domain.GasLogEntry
@@ -13,14 +12,14 @@ class MainViewModel : ViewModel() {
             listOf(Vehicle(
                     "Honda",
                     "Accord",
-                    mutableListOf(FluidLogEntry(Calendar.getInstance().time, 127000, 80, FluidLogEntry.FluidType.OIL)),
-                    mutableListOf(FluidLogEntry(Calendar.getInstance().time, 127000, 10, FluidLogEntry.FluidType.COOLANT)),
+                    mutableListOf(FluidLogEntry(Calendar.getInstance().time, 127000, 80)),
+                    mutableListOf(FluidLogEntry(Calendar.getInstance().time, 127000, 10)),
                     mutableListOf(GasLogEntry(Calendar.getInstance().time, 127000, 13.0))
             ), Vehicle(
                     "Toyota",
                     "Camry",
-                    mutableListOf(FluidLogEntry(Calendar.getInstance().time, 50000, 30, FluidLogEntry.FluidType.OIL)),
-                    mutableListOf(FluidLogEntry(Calendar.getInstance().time, 70000, 65, FluidLogEntry.FluidType.COOLANT)),
+                    mutableListOf(FluidLogEntry(Calendar.getInstance().time, 50000, 30)),
+                    mutableListOf(FluidLogEntry(Calendar.getInstance().time, 70000, 65)),
                     mutableListOf(GasLogEntry(Calendar.getInstance().time, 90000, 2.0))
             )
             )
--- app/src/main/java/com/logger/car/androidcarmaintenanceapp/domain/FluidLogEntry.kt
+++ app/src/main/java/com/logger/car/androidcarmaintenanceapp/domain/FluidLogEntry.kt
@@ -5,9 +5,5 @@ import java.util.*
 data class FluidLogEntry(
         override val entryDate: Date,
         override val mileage: Int,
-        val level: Int,
-		val fluidType: FluidType
-): BaseLogEntry {
-
-	enum class FluidType {OIL, COOLANT}
-}
+        val level: Int
+): BaseLogEntry
